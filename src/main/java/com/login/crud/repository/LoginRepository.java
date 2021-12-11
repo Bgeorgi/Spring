5ab7f6268b26c7f9c6569e03c5bbfc8d0a25ea6b
Bgeorgi <43473918+Bgeorgi@users.noreply.github.com>
2021-12-11 20:07:48 +0100

    Add files via upload

---
+++ src/main/java/com/login/crud/repository/LoginRepository.java
@@ -0,0 +1,9 @@
+package com.login.crud.repository;
+
+import com.login.crud.model.Login;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface LoginRepository
+        extends JpaRepository<Login,Integer> {
+}
